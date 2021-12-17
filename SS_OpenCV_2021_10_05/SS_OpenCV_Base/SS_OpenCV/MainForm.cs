991197495e01915e5c13b71c87798e24fd0b5cb1
johnny0111 <jmfde.carvalho@campus.fct.unl.pt>
2021-12-17 23:26:49 +0000

    Stage 2 Done

--- SS_OpenCV_2021_10_05/SS_OpenCV_Base/SS_OpenCV/MainForm.cs
+++ SS_OpenCV_2021_10_05/SS_OpenCV_Base/SS_OpenCV/MainForm.cs
@@ -585,6 +585,24 @@ namespace SS_OpenCV
 
             Cursor = Cursors.Default; // normal cursor
         }
+
+        private void sobelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            ImageClass.Sobel(img, imgUndo);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor
+        }
     }
 
 
